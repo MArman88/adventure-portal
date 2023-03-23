eae45815a8630dd3e2d275c0551faff1d9d94174
A. N. M. Mehbube Arman <MArman88@users.noreply.github.com>
2023-03-24 05:39:01 +0600

    Add files via upload

---
+++ script/index-script.js
@@ -0,0 +1,40 @@
+var fading = false;
+
+window.onscroll = function () {
+    console.log(window.scrollY);
+    if (window.scrollY < 100) {
+        fade(document.getElementById("over-nav"));
+    } else {
+        unfade(document.getElementById("over-nav"));
+    }
+}
+
+function fade(element) {
+    if (fading) { return }
+    fading = true
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1) {
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 20);
+}
+
+function unfade(element) {
+    if (!fading) { return }
+    fading = false
+    var op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1) {
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 20);
+}
